5b06eba754c8b2a7b0fa9c4848a4afd77179131c
JonBrelje <jonathan.brelje@gmail.com>
2017-01-09 22:07:36 -0500

    More nav bar stuff

--- static/scripts/float-panel.js
+++ static/scripts/float-panel.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
 
-	$('.mdl-layout').scroll(function(){
+	var nav_top = console.log($('#nav-bar').scrollTop())
+	$('.mdl-layout').scroll(function(nav_top){
 		console.log($('.mdl-layout').scrollTop())
 		var bottom = $('#nav-bar').getBoundingClientRect().bottom;
 
-	    if ($('.mdl-layout').scrollTop() > bottom) {
+	    if ($('.mdl-layout').scrollTop() > nav_top) {
 	      $('#nav-bar').addClass('navbar-fixed');
 	    }
-	    if ($('.mdl-layout').scrollTop() < bottom) {
+	    if ($('.mdl-layout').scrollTop() < nav_top) {
 	      $('#nav-bar').removeClass('navbar-fixed');
 	    }
 	});
