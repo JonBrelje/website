08b87dc6acd64c77085548f60285149ce906c126
JonBrelje <jonathan.brelje@gmail.com>
2017-01-09 21:47:43 -0500

    Still working on the java script

--- static/scripts/float-panel.js
+++ static/scripts/float-panel.js
@@ -1,15 +1,13 @@
-$(document).ready(function() {
-  
-  $('.mdl-layout__content').scroll(function () {
-      //if you hard code, then use console
-      //.log to determine when you want the 
-      //nav bar to stick.  
-     console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 280) {
-      $('#nav-bar').addClass('navbar-fixed');
-    }
-    if ($(window).scrollTop() < 281) {
-      $('#nav-bar').removeClass('navbar-fixed');
-    }
-  });
+jQuery(document).ready(function(){
+
+	jQuery('.mdl-layout__content').scroll(function(){
+		console.log('scrolled');
+		console.log($(window).scrollTop())
+	    if ($(window).scrollTop() > 280) {
+	      $('#nav-bar').addClass('navbar-fixed');
+	    }
+	    if ($(window).scrollTop() < 281) {
+	      $('#nav-bar').removeClass('navbar-fixed');
+	    }
+	});
 });
