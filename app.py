3fc3c313946ae359ac8ce4e99f2a15a9e868a49f
JonBrelje <jonathan.brelje@gmail.com>
2017-01-15 22:33:01 -0500

    Added a modal popup for email, beginning to play with sendgrid to acutally send the emails

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
+import os
+import sendgrid
+from sendgrid.helpers.mail import *
+
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
+def home():
     return render_template('home.html')
+
+@app.route('/_email_form')
+def _eamil_form():
+	sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+	from_email = Email("test@example.com")
+	subject = "Hello World from the SendGrid Python Library!"
+	to_email = Email("jonathan.brelje@gmail.com")
+	content = Content("text/plain", "Hello, Email!")
+	mail = Mail(from_email, subject, to_email, content)
+	response = sg.client.mail.send.post(request_body=mail.get())
+	print(response.status_code)
+	print(response.body)
+	print(response.headers)
+	return render_template('email_form.html')
 
 if __name__ == '__main__':
     app.run()
---
+++ static/scripts/mdl-modal.js
@@ -0,0 +1,71 @@
+/*Adapted from github oRRs/mdl-jquery-modal-dialog*/
+
+$(document).ready(function(){
+  $('#show-email').click(function() {
+    $.ajax({
+      url: '/_email_form',
+      success: function (data) {
+         showDialog({
+            title: 'Send me a message',
+            html: data,
+            negative: {
+              title: 'Nope'
+            },
+          }); 
+        },
+      });     
+  });
+});
+
+
+function showDialog(options) {
+  options = $.extend({
+    id: "modal-dialog",
+    title: null,
+    html: null,
+    negative: false,
+    positive: false,
+    cancelable: true,
+    contentStyle: null,
+    onLoaded: false
+  }, options);
+
+  // remove existing dialogs
+  $('.dialog-container').remove();
+  $(document).unbind("keyup.dialog");
+
+
+  $("body").append(options.html)
+  var dialog = $('#modal-dialog');
+  var content = dialog.find('.mdl-card');
+  componentHandler.upgradeDom();
+  if (options.cancelable) {
+    dialog.click(function() {
+      hideDialog(dialog);
+    });
+    $(document).bind("keyup.dialog", function(e) {
+      if (e.which == 27)
+        hideDialog(dialog);
+    });
+    content.click(function(e) {
+      e.stopPropagation();
+    });
+  }
+  setTimeout(function() {
+    dialog.css({
+      opacity: 1
+    });
+    if (options.onLoaded)
+      options.onLoaded();
+  }, 1);
+}
+
+function hideDialog(dialog) {
+  $(document).unbind("keyup.dialog");
+  dialog.css({
+    opacity: 0
+  });
+  setTimeout(function() {
+    dialog.remove();
+  }, 400);
+}
